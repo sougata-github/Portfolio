9bfa3f524925fa0647c99fcda234a42cf0b3d82c
Sougata <sougatadas9874@gmail.com>
2024-06-10 18:39:10 +0530

    delete skills section.

--- lib/data.ts
+++ lib/data.ts
@@ -2,7 +2,6 @@ export const links = [
   { name: "home", path: "/" },
   { name: "about", path: "/about" },
   { name: "projects", path: "/projects" },
-  { name: "skills", path: "/skills" },
   { name: "contact", path: "/contact" },
 ];
 
