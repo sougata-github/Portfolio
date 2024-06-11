3e08f73f80ee9bc7a2c118dd9ebde24430001bd8
Sougata <sougatadas9874@gmail.com>
2024-06-11 19:06:44 +0530

    add projects data.

--- lib/data.ts
+++ lib/data.ts
@@ -50,3 +50,36 @@ export const introduction = [
 
   "Whether it's discussing a project, sharing insights, or just having a good tech chat, I'm always open to new connections and opportunities.",
 ];
+
+export const projectsData = [
+  {
+    id: 1,
+    title: "DevOverFlow",
+    descriptiton:
+      "A community-driven Q&A platform for programming enthusiasts.",
+    live: "https://devoverflow-next.vercel.app/",
+    github: "https://github.com/sougata-github/DevOverFlow",
+  },
+  {
+    id: 2,
+    title: "Jotion",
+    descriptiton: "Productivity and note-taking web application.",
+    live: "https://jotion-next.vercel.app/",
+    github: "https://github.com/sougata-github/Jotion",
+  },
+  {
+    id: 3,
+    title: "Team Chat",
+    descriptiton: "Full-Stack Discord clone built using Next.js.",
+    live: "https://team-chat-efq7.onrender.com/invite/4a8a83a6-f6df-4644-90ee-d2c1093c0411",
+    github: "https://github.com/sougata-github/Team-Chat",
+  },
+  {
+    id: 4,
+    title: "iPhone Landing Page",
+    descriptiton:
+      "iPhone 15 Pro landing page built using React, GSAP and Three.js.",
+    live: "https://iphone-15-pro-landing-page.vercel.app/",
+    github: "https://github.com/sougata-github/iphone-15-pro-landing-page",
+  },
+];
